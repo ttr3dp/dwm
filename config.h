aacc6d94d3c5c7f56b445b670f7941e0563b3174
ttr3dp <git@ttr3dp.net>
2021-02-02 02:03:19 +0100

    Change font size

--- config.h
+++ config.h
@@ -10,7 +10,7 @@ static int swallowfloating    = 0;                             /* 1 means swallo
 static int focusonwheel       = 0;
 static int showbar            = 1;                             /* 0 means no bar */
 static int topbar             = 1;                             /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
+static const char *fonts[]          = { "monospace:pixelsize=14" };
 static const char dmenufont[]       = "monospace:size=10";
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
