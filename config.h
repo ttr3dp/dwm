697c461449f06c6b7bdcd651f510ddd9cce78575
ttr3dp <git@ttr3dp.net>
2021-02-04 02:08:06 +0100

    UNPATCH: xresources

--- config.h
+++ config.h
@@ -3,25 +3,24 @@
 #define TERMCLASS "St"
 
 /* appearance */
-static unsigned int borderpx  = 1;                             /* border pixel of windows */
-static unsigned int gappx     = 6;                             /* gaps between windows */
-static unsigned int snap      = 32;                            /* snap pixel */
-static int swallowfloating    = 0;                             /* 1 means swallow floating windows by default */
-static int focusonwheel       = 0;
-static int showbar            = 1;                             /* 0 means no bar */
-static int topbar             = 1;                             /* 0 means bottom bar */
+static const unsigned int borderpx  = 3;                             /* border pixel of windows */
+static const unsigned int gappx     = 12;                             /* gaps between windows */
+static const unsigned int snap      = 32;                            /* snap pixel */
+static const int swallowfloating    = 0;                             /* 1 means swallow floating windows by default */
+static const int focusonwheel       = 0;
+static const int showbar            = 1;                             /* 0 means no bar */
+static const int topbar             = 1;                             /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:pixelsize=14" };
 static const char dmenufont[]       = "monospace:size=10";
-static char normbgcolor[]           = "#222222";
-static char normbordercolor[]       = "#444444";
-static char normfgcolor[]           = "#bbbbbb";
-static char selfgcolor[]            = "#eeeeee";
-static char selbordercolor[]        = "#005577";
-static char selbgcolor[]            = "#005577";
-static char *colors[][3] = {
-       /*               fg           bg           border   */
-       [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
-       [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
+static const char col_gray1[]       = "#222222";
+static const char col_gray2[]       = "#444444";
+static const char col_gray3[]       = "#bbbbbb";
+static const char col_gray4[]       = "#eeeeee";
+static const char col_cyan[]        = "#005577";
+static const char *colors[][3]      = {
+	/*               fg         bg         border   */
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
 
 /* tagging */
@@ -39,9 +38,9 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
-static int nmaster     = 1;    /* number of clients in master area */
-static int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const float mfact     = 0.50; /* factor of master area size [0.05..0.95] */
+static const int nmaster     = 1;    /* number of clients in master area */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
 
 #include "tatami.c"
@@ -75,27 +74,6 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmrun", NULL };
 static const char *termcmd[]  = { TERMINAL, NULL };
-
-
-ResourcePref resources[] = {
-		{ "normbgcolor",        STRING,  &normbgcolor },
-		{ "normbordercolor",    STRING,  &normbordercolor },
-		{ "normfgcolor",        STRING,  &normfgcolor },
-		{ "selbgcolor",         STRING,  &selbgcolor },
-		{ "selbordercolor",     STRING,  &selbordercolor },
-		{ "selfgcolor",         STRING,  &selfgcolor },
-		{ "borderpx",           INTEGER, &borderpx },
-		{ "snap",               INTEGER, &snap },
-		{ "showbar",            INTEGER, &showbar },
-		{ "topbar",             INTEGER, &topbar },
-		{ "nmaster",            INTEGER, &nmaster },
-		{ "resizehints",        INTEGER, &resizehints },
-		{ "mfact",              FLOAT,   &mfact },
-		{ "swallowfloating",    INTEGER, &swallowfloating },
-		{ "gappx",              INTEGER, &gappx },
-		{ "focusonwheel",       INTEGER, &focusonwheel },
-};
-
 
 #include <X11/XF86keysym.h>
 
