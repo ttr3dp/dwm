fbb5098243666fb5a1c0c41a7ef3da29eec9569c
ttr3dp <git@ttr3dp.net>
2021-09-18 16:32:01 +0200

    Initial configuration

--- config.h
+++ config.h
@@ -45,7 +45,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -57,23 +57,25 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmrun", NULL };
 static const char *termcmd[]  = { "st", NULL };
+
+#include <X11/XF86keysym.h>
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
+	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
@@ -94,7 +96,20 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|ShiftMask,             XK_BackSpace, quit,       {0} },
+	/* -------------------------------- CUSTOM ------------------------------------- */
+	{ MODKEY|ShiftMask,   XK_x,     spawn,     SHCMD("lockscreen") },
+	{ 0, XK_Print,                  spawn,     SHCMD("maim -s $XDG_DESKTOP_DIR/Screenshot-$(date +%s).png && notify-send \\\"Screenshot taken!\\\"") },
+	{ 0, XF86XK_AudioRaiseVolume,   spawn,     SHCMD("vol +5") },
+	{ 0, XF86XK_AudioLowerVolume,   spawn,     SHCMD("vol -5") },
+	{ 0, XF86XK_AudioPlay,          spawn,     SHCMD("cmus-remote -u") },
+	{ 0, XF86XK_AudioStop,          spawn,     SHCMD("cmus-remote -s") },
+	{ 0, XF86XK_AudioNext,          spawn,     SHCMD("cmus-remote -n") },
+	{ 0, XF86XK_AudioPrev,          spawn,     SHCMD("cmus-remote -r") },
+	{ MODKEY, XK_grave,             spawn,     SHCMD("emoji") },
+	{ MODKEY|ControlMask, XK_p,     spawn,     SHCMD("passmenu") },
+	{ MODKEY|ShiftMask,   XK_p,     spawn,     SHCMD("ytpd") },
+	{ MODKEY,             XK_n,     spawn,     SHCMD("note") },
 };
 
 /* button definitions */
